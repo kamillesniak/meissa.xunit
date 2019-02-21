2158d25f09327b21b2eea664d1bef7bc1bbc6957
kamillesniak <kamillesniak02@gmail.com>
2019-02-21 20:04:01 +0100

    Sample xunit tests

---
+++ Meissa.XUnit.Tests/SampleFactTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Meissa.XUnit.Tests
+{
+    public class SampleFactTests
+    {
+        [Fact]
+        public void Fact_Test1()
+        {
+
+        }
+
+        [Fact]
+        public void Fact_Test2()
+        {
+
+        }
+    }
+}
---
+++ Meissa.XUnit.Tests/SampleTheoryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Meissa.XUnit.Tests
+{
+    public class SampleTheoryTests
+    {
+        [Theory]
+        [InlineData(1)]
+        public void Theory_test_1(int number)
+        {
+             
+        }
+        [Theory]
+        [InlineData(1)]
+        public void Theory_test_2(int number)
+        {
+
+        }
+    }
+}
