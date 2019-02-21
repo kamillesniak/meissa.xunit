a44a8353840269bac5dc3e72e41e75410b78248e
kamillesniak <kamillesniak02@gmail.com>
2019-02-21 20:05:03 +0100

    Tests for xunit runner

---
+++ Meissa.XUnit.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Meissa.XUnit.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Meissa.XUnit.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a1a21f4-aa8f-4003-a533-697b461269a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Meissa.XUnit.UnitTests/XunitRunnerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Meissa.xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Meissa.XUnit.UnitTests
+{
+    [TestClass]
+    public class XunitRunnerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var test = Directory.GetCurrentDirectory() + @"\Meissa.XUnit.SampleTests.dll";
+            var runner = new NativeTestsRunnerTestCasesPluginService();
+            runner.ExtractAllTestCasesFromTestLibrary(test);
+            
+        }
+    }
+}
